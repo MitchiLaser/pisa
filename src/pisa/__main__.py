d3178798382b4ec6dfd69f6ad6c7e2dcc909013e
Michael Hohenstein <michael@hohenste.in>
2024-05-07 00:09:07 +0200

    cli update: dry run (re)creates assignment file

--- src/pisa/__main__.py
+++ src/pisa/__main__.py
@@ -22,7 +22,7 @@ def main():
     parser.add_argument("-c", "--cluster", required=True, help="cluster configuration file")
     parser.add_argument("-t", "--task", required=True, help="task configuration file")
     parser.add_argument("-l", "--log", action="store_true", help="enable debug log printing to the console")
-    parser.add_argument("-d", "--dry-run", action="store_true", help="do not run anything, only show which tasks would be run")
+    parser.add_argument("-d", "--dry-run", action="store_true", help="do not run anything, only show which tasks would be run and create assignment file (useful for recreation of assignment file)")
     args = parser.parse_args()
 
     # set up log
